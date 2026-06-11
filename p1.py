3fb363c7db66de462214280503300ca2e9ccff45
Harsh <bharadwajh1005@gmail.com>
2026-06-11 07:48:25 +0530

    version-2

--- p1.py
+++ p1.py
@@ -3,3 +3,14 @@ if str(a)==str(a)[::-1]:
     print(a,"its palandrom")
 else:
     print(a,"its not palandrom")
+
+
+numbers = list(range(-25, 26))
+
+positive_numbers = [num for num in numbers if num > 0]
+negative_numbers = [num for num in numbers if num < 0]
+zero = [num for num in numbers if num == 0]
+
+print("Positive:", positive_numbers)
+print("Negative:", negative_numbers)
+print("zero:" , zero)
